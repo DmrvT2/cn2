69a1501908c4e13438513c7c984f8dbf94fe2698
DmrvT2 <148194801+DmrvT2@users.noreply.github.com>
2024-10-02 23:05:59 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,20 @@
+/**
+ * 名探偵コナンのテーマ（伴奏）
+ */
+/**
+ * 休符（1拍）
+ */
+/**
+ * 休符（1/2拍）
+ */
+input.onButtonPressed(Button.A, function () {
+    music.play(music.tonePlayable(0, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(175, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(175, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(175, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(175, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(156, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(156, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(156, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(156, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+})
